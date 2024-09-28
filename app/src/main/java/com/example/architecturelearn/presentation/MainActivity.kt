8bd93c10090810930c17149b7bca9037e72a0595
Nikolay (nazarovNV) <nikolaynazarovvv216@gmail.com>
2024-09-28 19:47:34 +0300

    Применение UseCase

--- app/src/main/java/com/example/architecturelearn/presentation/MainActivity.kt
+++ app/src/main/java/com/example/architecturelearn/presentation/MainActivity.kt
@@ -6,8 +6,14 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import com.example.architecturelearn.R
+import com.example.architecturelearn.domain.models.SaveUserNameParam
+import com.example.architecturelearn.domain.models.UserName
+import com.example.architecturelearn.domain.usecase.GetUserNameUseCase
+import com.example.architecturelearn.domain.usecase.SaveUserNameUseCase
 
 class MainActivity : Activity() {
+    private val getUserNameUseCase = GetUserNameUseCase()
+    private val saveUserNameUseCase = SaveUserNameUseCase()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,5 +22,18 @@ class MainActivity : Activity() {
         val dataEditView = findViewById<EditText>(R.id.dataEditText)
         val sendButton = findViewById<Button>(R.id.sendButton)
         val receiveButton = findViewById<Button>(R.id.receiveButton)
+
+        sendButton.setOnClickListener {
+            val text = dataEditView.text.toString()
+            val param = SaveUserNameParam(text)
+            val result : Boolean = saveUserNameUseCase.execute(param)
+            dataTextView.text = "Save result = $result"
+
+        }
+        receiveButton.setOnClickListener {
+            val userName : UserName = getUserNameUseCase.execute()
+            dataTextView.text = "${userName.firstName} ${userName.lastName}"
+
+        }
     }
 }
