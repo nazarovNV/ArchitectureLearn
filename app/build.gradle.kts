96e587b46f4065b2bcfbbe8a4e50b651e1a8e087
nikolayNazarov <nazarov.nikolay15@wb.ru>
2024-11-23 20:30:30 +0300

    unit test for viewmodel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ android {
     }
 }
 
+
+
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -60,7 +62,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
--- app/src/test/java/com/example/architecturelearn/presentation/MainViewModelTest.kt
+++ app/src/test/java/com/example/architecturelearn/presentation/MainViewModelTest.kt
@@ -1,24 +1,53 @@
 package com.example.architecturelearn.presentation
 
-import org.junit.Test
+import com.example.architecturelearn.domain.models.SaveUserNameParam
+import com.example.architecturelearn.domain.usecase.GetUserNameUseCase
+import com.example.architecturelearn.domain.usecase.SaveUserNameUseCase
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
 
 class MainViewModelTest {
+    private val getUserNameUseCase = mock<GetUserNameUseCase>()
+    private val saveUserNameUseCase = mock<SaveUserNameUseCase>()
+    private lateinit var viewModel: MainViewModel
 
     @AfterEach
-    fun afterEach(){
-
+    fun afterEach() {
+        Mockito.reset(getUserNameUseCase)
+        Mockito.reset(saveUserNameUseCase)
     }
 
     @BeforeEach
-    fun beforeEach(){
-
+    fun beforeEach() {
+        viewModel = MainViewModel(
+            getUserNameUseCase = getUserNameUseCase,
+            saveUserNameUseCase = saveUserNameUseCase
+        )
     }
 
     @Test
     fun `should save username and return true`() {
+        val testSaveText = "Test user name"
+        val saveResult = true
 
+        val testParams = SaveUserNameParam(name = testSaveText)
+
+        Mockito.`when`(saveUserNameUseCase.execute(param = testParams)).thenReturn(saveResult)
+
+
+        viewModel.save(text = testSaveText)
+
+        val expected = "Save result = true"
+        val actual = viewModel.resultLive.value
+
+        Mockito.verify(saveUserNameUseCase, times(numInvocations = 1)).execute(param = testParams)
+
+        Assertions.assertEquals(expected, actual)
     }
 
     @Test
