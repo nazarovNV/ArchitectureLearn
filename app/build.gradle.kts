2014be06b83dc1cf9c4512d5ab8bc44449bc2a74
nikolayNazarov <nazarov.nikolay15@wb.ru>
2024-11-24 00:53:55 +0300

    unit test for viewmodel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,12 +75,16 @@ dependencies {
     kapt(libs.hilt.android.compiler)
     implementation(libs.androidx.fragment.ktx)
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 
-
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    testImplementation("org.mockito:mockito-core:4.0.0")
     testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
+
+    testImplementation("org.mockito:mockito-inline:4.0.0")
+
+
+    // testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    // testImplementation("org.mockito:mockito-core:4.0.0")
+
 
     implementation("com.google.android.material:material:1.12.0")
     implementation(project(":domain"))
