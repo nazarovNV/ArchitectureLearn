b15ce27e2b0227e1d711744adaee66760ffc08f1
nikolayNazarov <nazarov.nikolay15@wb.ru>
2024-12-17 23:31:20 +0300

    unit test for viewmodel

--- app/src/main/java/com/example/architecturelearn/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/architecturelearn/presentation/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.architecturelearn.presentation
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
